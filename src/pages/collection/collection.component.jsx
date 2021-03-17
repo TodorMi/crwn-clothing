53dcf5d0ef6d64f43acd3b408200279c93bae704
TodorMi <46191374+TodorMi@users.noreply.github.com>
2021-03-17 15:56:11 +0200

    updated our application App component, sign-in and sign-up component to use hooks

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -3,7 +3,7 @@ import "./collection.styles.scss";
 import { connect } from "react-redux";
 import { selectCollection } from "../../redux/shop/shop.selectors";
 
-import CollectionItem  from "../../components/collection-item/collection-item.component";
+import CollectionItem from "../../components/collection-item/collection-item.component";
 
 const CollectionPage = ({ collection }) => {
   const { title, items } = collection;
