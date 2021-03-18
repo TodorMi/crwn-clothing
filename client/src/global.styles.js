e15526b7401624015defcfc2f07dbab83aec9dd6
TodorMi <46191374+TodorMi@users.noreply.github.com>
2021-03-18 17:04:53 +0200

    partly mobile-friendly

---
+++ client/src/global.styles.js
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+body {
+
+
+  font-family: "Open Sans Condensed" !important;
+  padding: 20px 60px;
+
+  
+  @media screen and (max-width: 900px){
+        padding: 10px;
+    }
+  
+}
+
+a {
+  text-decoration: none;
+  color: black;
+}
+
+* {
+  box-sizing: border-box;
+}
+`;
