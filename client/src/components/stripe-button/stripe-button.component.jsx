b5f00a50ca35c4fcb9c51529967cd913e34872c3
TodorMi <46191374+TodorMi@users.noreply.github.com>
2021-03-18 11:49:41 +0200

    separating our cra app into client folder, added express server to help handle backend stripe payment

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,17 +1,32 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
-import Logo from '../../assets/crown.svg'
+import Logo from "../../assets/crown.svg";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey =
     "pk_test_51IVAh0EiRmpMwH9NtU7QwLuKoVyODCTU6UBrYECd0x1F3ZBRZ8pGETQuFX7qeAl6Pbf2IlmHbXtMdMhasvl6Ba0z00kfzHlv4P";
 
-  const onToken = (token) => {
-    console.log(token);
-    alert("Payment successful");
-  };
-
+    const onToken = (token) => {
+      axios({
+        url: "payment",
+        method: "post",
+        data: {
+          amount: priceForStripe,
+          token,
+        },
+      })
+        .then((response) => {
+          alert("Payment successful!");
+        })
+        .catch((error) => {
+          console.log("Payment error: ", JSON.parse(error));
+          alert(
+            "There was an issue with your payment. Please make sure you use the provided credit card!"
+          );
+        });
+    };
   return (
     <StripeCheckout
       label="Pay Now"
