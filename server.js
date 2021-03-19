d1582ebeb18fd0e00bda0486ec289937ec307007
TodorMi <46191374+TodorMi@users.noreply.github.com>
2021-03-19 13:11:18 +0200

    idk

--- server.js
+++ server.js
@@ -13,23 +13,21 @@ const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(enforce.HTTPS({ trustProtoHeader: true }));
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
-
-  app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+app.use(cors());
+ 
+if (process.env.NODE_ENV === 'production') {
+  app.use(compression);
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
+  app.use(express.static(path.join(__dirname, 'client/build')));
+ 
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
-
-app.listen(port, (error) => {
+ 
+app.listen(port, error => {
   if (error) throw error;
-  console.log("Server running on port " + port);
-});
-
-app.get("/service-worker.js", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "..", "build", "service-worker.js"));
+  console.log('Server is running on port ' + port);
 });
 
 app.post("/payment", (req, res) => {
